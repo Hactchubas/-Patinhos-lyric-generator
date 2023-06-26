1d155450f1411cbad86a7575945ea4ba43616b88
Kauã S. Maia <116105882+Hactchubas@users.noreply.github.com>
2023-06-26 11:33:19 -0300

    Melhor código
    Melhorei o código do patinhos.py pra não serem necessárias tantas verificações.

--- patinhos/foram_passear/patinhos.py
+++ patinhos/foram_passear/patinhos.py
@@ -3,23 +3,16 @@ from num2words import num2words
 def letra_gerador(n):
     n_patinhos = n
     letra = ''
-    for pato in range(n_patinhos,0,-1):
+    for pato in range(n_patinhos,2,-1):
         pato_extenso = num2words(pato, lang='pt_BR').capitalize()
-        if(pato == 1):
-            pato_txt = "nenhum patinho voltou"
-            letra += f"<p>{pato_extenso} patinho foi passear <br>Além das montanhas para brincar<br>A mamãe gritou: 'Quá quá quá quá'<br>Mas {pato_txt} de lá <p>"
-            continue
-        elif (pato == 2):
-            pato_txt = "só um patinho voltou"
-        else:
-            pato_txt = f"só {num2words(pato-1, lang='pt_BR')} patinhos voltaram"
-
+        pato_txt = f"só {num2words(pato-1, lang='pt_BR')} patinhos voltaram"
         letra += f"<p>{pato_extenso} patinhos foram passear<br>Além das montanhas para brincar<br>A mamãe gritou: 'Quá quá quá quá'<br>Mas {pato_txt} de lá<p>"
-
-    letra += "<p>Poxa, a mamãe patinha ficou tão triste naquele dia<br>Aonde será que estavam os seus filhotinhos?<br>Mas essa história vai ter um final feliz <br>Sabe por quê?<p>" 
+    
+    letra += "<p>Dois patinhos foram passear <br>Além das montanhas para brincar<br>A mamãe gritou: 'Quá quá quá quá'<br>Mas só um patinho voltou de lá <p><p>Um patinho foi passear <br>Além das montanhas para brincar<br>A mamãe gritou: 'Quá quá quá quá'<br>Mas nenhum patinho voltou de lá <p><p>Poxa, a mamãe patinha ficou tão triste naquele dia<br>Aonde será que estavam os seus filhotinhos?<br>Mas essa história vai ter um final feliz <br>Sabe por quê?<p>" 
+    
     if(n_patinhos == 1):
         pato_txt = "nenhum patinho voltou"
-        letra += f"<p>A mamãe patinha foi procurar<br>Além das montanhas, na beira do mar<br>A mamãe gritou: 'Quá quá quá quá'<br>E o seu patinho voltou de lá<p>" 
+        letra += f"<p>A mamãe patinha foi procurar<br>Além das montanhas, na beira do mar<br>A mamãe gritou: 'Quá quá quá quá'<br>E o patinho voltou de lá<p>" 
     else:
          letra += f"<p>A mamãe patinha foi procurar<br>Além das montanhas, na beira do mar<br>A mamãe gritou: 'Quá quá quá quá'<br>E os {num2words(n_patinhos, lang='pt_BR')} patinhos voltaram de lá<p>"    
     
--- patinhos/foram_passear/views.py
+++ patinhos/foram_passear/views.py
@@ -9,12 +9,16 @@ def gerador(request):
     n_patinhos = request.GET.get('patinhos')
     if n_patinhos is not '':
         n_patinhos = int(n_patinhos)
-        if n_patinhos <= 0 and not '':
+        if n_patinhos <= 0:
             return render(request, "foram_passear/index.html",{
                 "message": "Por favor insira um valor inteiro positivo"
             })
         letra = letra_gerador(n_patinhos)
         n_patinhos = num2words(n_patinhos,lang="pt_BR").capitalize()
+        if n_patinhos != "Um": 
+            n_patinhos += " patinhos"
+        else:
+            n_patinhos += " patinho"
         return render(request, "foram_passear/index.html",{
             "n_patinhos": n_patinhos,
             "letra": letra,
